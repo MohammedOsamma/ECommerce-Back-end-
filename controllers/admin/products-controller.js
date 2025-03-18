04d3e020cc4d4cf646fc582526007b284d288640
MohammedOsamma <mo856915i@gmail.com>
2025-03-18 23:42:37 +0200

    Finish Add, Edit, Delete and Fetch All Products Functions in Product Controller & Add Their  Routes

--- controllers/admin/products-controller.js
+++ controllers/admin/products-controller.js
@@ -1,4 +1,5 @@
 const { imageUploadUtil } = require("../../helper/cloudinary");
+const Product = require("../../models/Product");
 
 const handleImageUpload = async (req, res) => {
   try {
@@ -18,4 +19,128 @@ const handleImageUpload = async (req, res) => {
   }
 };
 
-module.exports = { handleImageUpload };
+// add new product
+
+const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    });
+    await newlyCreatedProduct.save();
+    res.status(200).json({
+      success: true,
+      data: newlyCreatedProduct,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured ",
+    });
+  }
+};
+// fetch all product
+const fetchAllProducts = async (req, res) => {
+  const listOfProducts = await Product.find({});
+  res.status(200).json({
+    success: true,
+    data: listOfProducts,
+  });
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured ",
+    });
+  }
+};
+// edit product
+const editProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const findProdcut = await Product.findById(id);
+
+    if (!findProdcut)
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+
+    findProdcut.title = title || findProdcut.title;
+    findProdcut.description = description || findProdcut.description;
+    findProdcut.category = category || findProdcut.category;
+    findProdcut.brand = brand || findProdcut.brand;
+    findProdcut.price = price || findProdcut.price;
+    findProdcut.salePrice = salePrice || findProdcut.salePrice;
+    findProdcut.totalStock = totalStock || findProdcut.totalStock;
+    findProdcut.image = image || findProdcut.image;
+
+    await findProdcut.save();
+    res.status(200).json({ success: true, data: findProdcut });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured ",
+    });
+  }
+};
+// delete product
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured ",
+    });
+  }
+};
+module.exports = {
+  handleImageUpload,
+  addProduct,
+  fetchAllProducts,
+  editProduct,
+  deleteProduct,
+};
--- routes/admin/products-route.js
+++ routes/admin/products-route.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 const {
   handleImageUpload,
+  addProduct,
+  editProduct,
+  deleteProduct,
+  fetchAllProducts,
 } = require("../../controllers/admin/products-controller");
 const { upload } = require("../../helper/cloudinary");
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+router.post("/add", addProduct);
+router.put("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
+router.get("/get", fetchAllProducts);
 
 module.exports = router;

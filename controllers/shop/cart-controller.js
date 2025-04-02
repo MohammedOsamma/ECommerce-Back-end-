cf1c2152c614c6dc32eb6dd894fa711d2f0746b7
MohammedOsamma <mo856915i@gmail.com>
2025-04-02 20:52:59 +0200

    Add fetchCartItems Function

--- controllers/shop/cart-controller.js
+++ controllers/shop/cart-controller.js
@@ -49,6 +49,62 @@ const addToCart = async (req, res) => {
   }
 };
 
+const fetchCartItem = async (req, res) => {
+  try {
+    const { userId } = req.params();
+    if (!userId) {
+      return res.status(400).josn({
+        success: false,
+        message: "User id is manadatory!",
+      });
+    }
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "item.productId",
+      select: "image title price salePrice",
+    });
+
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found!",
+      });
+    }
+
+    const validItems = cart.items.filter(
+      (productItem) => productItem.productId
+    );
+
+    if (validItems.length < cart.items.lemgth) {
+      cart.items = validItems;
+      await cart.save();
+    }
+
+    const populateCartItems = validItems.map((item) => ({
+      productId: item.productId._id,
+      image: item.productId.image,
+      title: item.productId.title,
+      price: item.productId.price,
+      salePrice: item.productId.salePrice,
+      quantity: item.productId.quantity,
+    }));
+
+    res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Some Error Occured",
+    });
+  }
+};
+
 module.exports = {
   addToCart,
+  fetchCartItem,
 };

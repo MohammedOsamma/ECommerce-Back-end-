9f2da8a788e9bfe30ffbdaca7ddad9ae745e9a4f
MohammedOsamma <mo856915i@gmail.com>
2025-04-02 19:18:08 +0200

    Add Cart Controller & finish addToCart function

---
+++ controllers/shop/cart-controller.js
@@ -0,0 +1,54 @@
+const Cart = require("../../models/Cart.");
+const Product = require("../../models/Product");
+
+const addToCart = async (req, res) => {
+  const { userId, productId, quantity } = req.body;
+  try {
+    if (!userId || !productId || quantity <= 0) {
+      return res.status(500).json({
+        success: false,
+        message: "Invalid Data Prodvided",
+      });
+    }
+
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found!",
+      });
+    }
+
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (findCurrentProductIndex === -1) {
+      cart.items.push({ productId, quantity });
+    } else {
+      cart.items[findCurrentProductIndex].quantity += quantity;
+    }
+
+    await cart.save();
+
+    res.status(200).json({
+      success: true,
+      data: cart,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Some Error Occured",
+    });
+  }
+};
+
+module.exports = {
+  addToCart,
+};

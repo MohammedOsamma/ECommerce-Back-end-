faa835a4f7b9f411f55a69d71c8cd17629f5dcfc
MohammedOsamma <mo856915i@gmail.com>
2025-04-14 01:00:47 +0200

    Add getAllOrderByUser , GetOrderDetails Functions with routes

--- controllers/shop/order-controller.js
+++ controllers/shop/order-controller.js
@@ -130,7 +130,58 @@ const capturePayment = async (req, res) => {
   }
 };
 
+const getAllOrderByUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const orders = await Order.find({ userId });
+
+    if (!orders.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No orders found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: orders,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
+
+const getOrderDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
 module.exports = {
   createOrder,
   capturePayment,
+  getAllOrderByUser,
+  getOrderDetails,
 };
--- routes/shop/order-routes.js
+++ routes/shop/order-routes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const {
   createOrder,
   capturePayment,
+  getAllOrderByUser,
+  getOrderDetails,
 } = require("../../controllers/shop/order-controller");
 
 router.post("/create", createOrder);
 router.post("/capture", capturePayment);
+router.get("/list/:userId", getAllOrderByUser);
+router.get("/details/:id", getOrderDetails);
 
 module.exports = router;

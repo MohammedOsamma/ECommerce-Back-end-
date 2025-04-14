58c0fd3805e8d387483e359da9dd39599191958d
MohammedOsamma <mo856915i@gmail.com>
2025-04-14 03:12:17 +0200

    add getOrderDetailsForAdmin function for order-controller

--- controllers/admin/order-controller.js
+++ controllers/admin/order-controller.js
@@ -24,6 +24,32 @@ const getAllOrderOfAllUser = async (req, res) => {
   }
 };
 
+const getOrderDetailsForAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
+
 module.exports = {
   getAllOrderOfAllUser,
+  getOrderDetailsForAdmin,
 };
--- routes/admin/order-routes.js
+++ routes/admin/order-routes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllOrderOfAllUser,
+  getOrderDetailsForAdmin,
 } = require("../../controllers/admin/order-controller");
 
 router.get("/get", getAllOrderOfAllUser);
+router.get("/details/:id", getOrderDetailsForAdmin);
 
 module.exports = router;

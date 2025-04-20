ebc988d66fb759e06bbe695b95cc3aa6531043d7
MohammedOsamma <mo856915i@gmail.com>
2025-04-20 03:39:06 +0200

    Add addProductReview function in review-controller

--- controllers/shop/review.controller.js
+++ controllers/shop/review.controller.js
@@ -2,6 +2,66 @@ const Order = require("../../models/Order");
 const Product = require("../../models/Product");
 const ProductReview = require("../../models/Review");
 
+const addProductReview = async (req, res) => {
+  try {
+    const { productId, userId, userName, reviewMessage, reviewValue } =
+      req.body;
+
+    const order = await Order.findOne({
+      userId,
+      "cartItems.productId": productId,
+    });
+
+    if (!order) {
+      return res.status(400).json({
+        success: false,
+        message: "You have not purchased this product",
+      });
+    }
+
+    const checkExistingReview = await ProductReview.findOne({
+      productId,
+      userId,
+    });
+
+    if (checkExistingReview) {
+      return res.status(400).json({
+        success: false,
+        message: "You already reviewed this product!",
+      });
+    }
+
+    const newReview = new ProductReview({
+      productId,
+      userId,
+      userName,
+      reviewMessage,
+      reviewValue,
+    });
+
+    await newReview.save();
+
+    const reviews = await ProductReview.find({ productId });
+    const totalReviewsLength = reviews.length;
+    const averageReview =
+      reviews.reduce((sum, reviewItem) => sum + reviewItem.reviewValue, 0) /
+      totalReviewsLength;
+
+    await Product.findByIdAndUpdate(productId, { averageReview });
+
+    res.status(201).json({
+      success: true,
+      data: newReview,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured ",
+    });
+  }
+};
+
 const getProductReviews = async (req, res) => {
   try {
     const { productId } = req.params;
@@ -21,5 +81,6 @@ const getProductReviews = async (req, res) => {
 };
 
 module.exports = {
+  addProductReview,
   getProductReviews,
 };
